45ede19a324b5d0288f0054a7f6ee00cc505c4bc
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-29 15:24:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 ROUTER_MODEL=$INPUT_ROUTER_MODEL
 COMPILE_CONFIG=$INPUT_COMPILE_CONFIG
+FILES_CONFIG=$INPUT_FILES_CONFIG
 
 echo -e "Total CPU cores\t: $(nproc)"
 cat /proc/cpuinfo | grep 'model name'
@@ -22,13 +23,46 @@ timedatectl set-timezone "Asia/Shanghai"
 df -h
 
 cd /github/workspace/
+echo '当前执行步骤：1-下载AgustinLorenzo/openwrt仓库'
 git clone --depth 1  https://github.com/thinkcyy/OpenWRT-Action OpenWRT-Action 
 git clone --depth 1  https://github.com/AgustinLorenzo/openwrt -b main --single-branch ./OpenWRT-Action/openwrt
 #echo "当前工作目录"
 #pwd
 #tree -L 3
- 
+
+echo '当前执行步骤：2-处理Package'
 cd /github/workspace/OpenWRT-Action
 chmod +x ./zhKong/scripts/*.sh
-cp -v ./zhKong/config/$INPUT_COMPILE_CONFIG.config ./zhKong/config/config-$INPUT_ROUTER_MODEL.config
+cp -vr ./zhKong/config/$INPUT_COMPILE_CONFIG.config ./zhKong/config/config-$INPUT_ROUTER_MODEL.config
 ./zhKong/scripts/prepare.sh
+
+echo '当前执行步骤：3-处理自定义配置files'
+cd /github/workspace/OpenWRT-Action
+cp -vr ../openwrt-config/$INPUT_ROUTER_MODEL/files ./openwrt/
+chmod +x ./openwrt/files/etc/tinc/tincvpn/tinc-up
+
+echo '当前执行步骤：4-编译'
+cd /github/workspace/OpenWRT-Action/openwrt
+echo '当前执行步骤：4.1-下载'
+make download -j$(nproc)
+echo '当前执行步骤：4.2-编译'
+make -j$(nproc) || make -j1 V=s
+
+echo '当前执行步骤：5-打标'
+tag_name=$(date +%Y%m%d-%H%M)
+echo $tag_name
+            
+echo '当前执行步骤：6-组织产出文件'
+cd /github/workspace/OpenWRT-Action
+rm -rf ./artifact/
+mkdir -p ./artifact/
+cp -vrf $(find ./openwrt/bin/targets/ -type f -name "*sysupgrade*") ./artifact/
+cp -vrf $(find ./openwrt/bin/targets/ -type f -name "*.buildinfo") ./artifact/
+cp -vr ./openwrt/.config ./artifact/defconfig-${{ env.COMPILE_CONFIG }}.config
+cd ./artifact/
+rename 's/sysupgrade.bin/sysupgrade-${{ env.COMPILE_CONFIG }}_${{ env.tag_name }}.bin/' *
+ls -Ahl
+
+
+
+
