565c5e3b29ebf60ed93e95c58292c20f4a3dbf41
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-29 10:08:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/g
 apt-get update && apt-get install -y git
 #wget -P /usr/local/sbin/ https://github.com/HiGarfield/lede-17.01.4-Mod/raw/master/.github/backup/apt-fast
 #chmod -R 755 /usr/local/sbin/apt-fast
-apt-get -y install zstd dwarves llvm clang lldb lld build-essential rsync asciidoc binutils bzip2 gawk gettext git libncurses5-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc-s1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils upx-ucl libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget ccache curl swig coreutils vim nano python3 python3-pip python3-ply haveged lrzsz scons libpython3-dev
+apt-get -y install tree zstd dwarves llvm clang lldb lld build-essential rsync asciidoc binutils bzip2 gawk gettext git libncurses5-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc-s1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils upx-ucl libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget ccache curl swig coreutils vim nano python3 python3-pip python3-ply haveged lrzsz scons libpython3-dev
 pip3 install pyelftools pylibfdt
 apt-get autoremove --purge
 apt-get clean
@@ -21,12 +21,12 @@ df -h
 
 cd /github/workspace/
 git clone --depth 1  https://github.com/thinkcyy/OpenWRT-Action OpenWRT-Action 
-git clone --depth 1  https://github.com/AgustinLorenzo/openwrt -b main --single-branch ./OpenWRT-Action/openwrt
+git clone --depth 1  https://github.com/AgustinLorenzo/openwrt -b main --single-branch ./openwrt
 echo "当前工作目录"
 pwd
 tree -L 3
  
 
 chmod +x ./OpenWRT-Action/zhKong/scripts/*.sh
-cp -v ./OpenWRT-Action/zhKong/config/$INPUT_ROUTER_MODEL.config ./OpenWRT-Action/zhKong/config/config-$INPUT_ROUTER_MODEL.config
+cp -v ./OpenWRT-Action/zhKong/config/$INPUT_COMPILE_CONFIGL.config ./OpenWRT-Action/zhKong/config/config-$INPUT_ROUTER_MODEL.config
 ./OpenWRT-Action/zhKong/scripts/prepare.sh
