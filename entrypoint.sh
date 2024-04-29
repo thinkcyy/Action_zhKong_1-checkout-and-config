13b49e17f41d8790f4dfd0f74213ff966ff64c75
thinkcyy <142167280+thinkcyy@users.noreply.github.com>
2024-04-29 09:28:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,11 @@
 echo -e "Total CPU cores\t: $(nproc)"
 cat /proc/cpuinfo | grep 'model name'
 ulimit -a 
+echo /etc/os-release
 
 swapoff -a
-rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
-apt update
-apt-get install git -y
+#rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
+apt-get update && apt-get install -y git
 #wget -P /usr/local/sbin/ https://github.com/HiGarfield/lede-17.01.4-Mod/raw/master/.github/backup/apt-fast
 #chmod -R 755 /usr/local/sbin/apt-fast
 apt-get -y install zstd dwarves llvm clang lldb lld build-essential rsync asciidoc binutils bzip2 gawk gettext git libncurses5-dev patch python3 python2.7 unzip zlib1g-dev lib32gcc-s1 libc6-dev-i386 subversion flex uglifyjs gcc-multilib p7zip p7zip-full msmtp libssl-dev texinfo libreadline-dev libglib2.0-dev xmlto qemu-utils upx-ucl libelf-dev autoconf automake libtool autopoint device-tree-compiler g++-multilib antlr3 gperf wget ccache curl swig coreutils vim nano python3 python3-pip python3-ply haveged lrzsz scons libpython3-dev
